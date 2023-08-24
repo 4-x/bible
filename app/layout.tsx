b03406505eb68a3a1916cb80325c97e4cd025865
Alexander Shoup <alexshoup@gmail.com>
2023-08-24 12:07:24 -0600

    design cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter_Tight } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter_Tight({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'KJV Bible',
