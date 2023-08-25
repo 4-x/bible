48d7792ace67e48eb9558c41e771a01b4fda20f4
Alexander Shoup <alexshoup@gmail.com>
2023-08-25 10:06:49 -0600

    fix random verse bug

--- app/[lang]/random/page.tsx
+++ app/[lang]/random/page.tsx
@@ -11,13 +11,12 @@ export default async function Page({ params: { lang } }: { params: { lang: strin
   const _bible = await fs.readFile(jsonDirectory + `/${langs[lang]}.json`, 'utf8')
   const _bibleJSON: BibleBook[] = JSON.parse(_bible.trim())
 
-  const randomBook = Math.floor(Math.random() * _bible.length)
+  const randomBook = Math.floor(Math.random() * _bibleJSON.length)
   const randomChapter = Math.floor(Math.random() * _bibleJSON[randomBook].chapters.length)
   const randomVerse = Math.floor(Math.random() * _bibleJSON[randomBook].chapters[randomChapter].length)
-  console.log(lang)
+  console.log(randomVerse)
   return (
     <>
-      <h1><Link href={`/${lang}`}>KJV Bible</Link></h1>
       <section className='random-verse'>
         <p>{_bibleJSON[randomBook].chapters[randomChapter][randomVerse]}</p>
         <p><cite><Link href={`/${lang}/book/${_bibleJSON[randomBook].abbrev}#_${randomChapter + 1}_${randomVerse + 1}`}>{_bibleJSON[randomBook].name} {randomChapter + 1}:{randomVerse + 1}</Link></cite></p>
@@ -28,7 +27,7 @@ export default async function Page({ params: { lang } }: { params: { lang: strin
 
 export async function generateStaticParams() {
   const staticParamsArr = locales.map(lang=>{
-    return {lang}
+    return {lang:lang}
   })
 
   return staticParamsArr
